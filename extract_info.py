e9950432f2e7abe9775ff75b2f2df193be4849b2
joanna_Cin <joanna@cinnamon.is>
2021-11-14 04:46:03 +0000

    Preprocess with adaptive threshold

--- extract_info.py
+++ extract_info.py
@@ -3,6 +3,7 @@ import argparse
 import cv2
 import os
 os.environ["TESSDATA_PREFIX"] = "./"
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 from PIL import Image
 from transformers import pipeline
@@ -12,16 +13,20 @@ answer_extractor = pipeline(
     model="ancs21/xlm-roberta-large-vi-qa",
 )
 template_questions = {
-    "animal_name": "Loài động vật nào được buôn bán?",
-    "product_name": "Sản phẩm được bán là gì?",
-    "post_date": "Thời gian rao bán?",
+    "animal_name": "Tên động vật được rao bán?",
+    "product_name": "Sản phẩm được rao bán là gì?",
+    "quantity": "Bao nhiêu sản phẩm được rao bán?",
+    "post_date": "Bài đăng vào thời gian nào?",
+    "post_location": "Địa điểm tổ chức rao bán?",
     "contact": "Số điện thoại người bán?",
 }
 
 def _extract_text(fpath):
     img = cv2.imread(fpath)
+    img = cv2.resize(img, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    gray = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+    # gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
     text = pytesseract.image_to_string(gray, lang='vie')
     return text
 
@@ -30,7 +35,7 @@ def _extract_details(context):
     details = {}
     for qid, question in template_questions.items():
         outputs = answer_extractor(question=question, context=context)
-        details[qid] = {"answer": outputs["answer"], "score": outputs["score"]}
+        details[qid] = {"answer": outputs["answer"].strip(), "score": outputs["score"]}
     return details
 
 def extract_info(fpath):
