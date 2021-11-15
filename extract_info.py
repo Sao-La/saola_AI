fe36f84d8683d350201572ad484e2d53553d46ac
btcnhung1299 <joanna@cinnamon.is>
2021-11-15 05:40:39 +0000

    Minor update

--- extract_info.py
+++ extract_info.py
@@ -5,7 +5,7 @@ import os
 import numpy as np
 import urllib.request
 os.environ["TESSDATA_PREFIX"] = "./resources/"
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
 import json
 import time
 
@@ -26,27 +26,14 @@ template_questions = {
     "location": "Địa điểm rao bán?",
     "phone": "Số điện thoại người bán?",
 }
-#print("Finish set-up")
 
 def url_to_image(url):
     with urllib.request.urlopen(url) as resp:
-        image = np.asarray(bytearray(resp.read()), dtype="uint8")
-        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-        return image
+        img = np.asarray(bytearray(resp.read()), dtype="uint8")
+        img = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        return img
 
-def _extract_text_with_url(url: str):
-    img = url_to_image(url)
-    img = cv2.resize(img, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # gray = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
-    gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-    text = pytesseract.image_to_string(gray, lang="vie")
-    # correction
-    lines = [utils.correct(x) for x in text.split("\n") if len(x.strip()) > 0]
-    return " \n ".join(lines)
-
-def _extract_text(fpath: str):
-    img = cv2.imread(fpath)
+def _extract_text(img):
     img = cv2.resize(img, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # gray = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
@@ -73,23 +60,16 @@ def _extract_details(context: str):
             details[qid]["answer"] = utils.parse_date(details[qid]["answer"])
     return details
 
-def extract_info(fpath: str):
-    assert os.path.exists(fpath), f"File doesn't exist: {fpath}"
-    context = _extract_text(fpath)
-    details = _extract_details(context)
-    return details
-
-def extract_info_with_url(url: str):
-    context = _extract_text_with_url(url)
+def extract_info(img):
+    context = _extract_text(img)
     details = _extract_details(context)
     return details
 
 def modified():
     [report, url] = open('input', 'r').read().strip().split(' ')
-    # print({"report": report, "content": url}, flush=True)
-    details = extract_info_with_url(url)
+    img = url_to_image(url)
+    details = extract_info(img)
     print({"report": report, "content": details}, flush=True)
-
 
 if __name__ == "__main__":
     prev = 0
@@ -97,23 +77,26 @@ if __name__ == "__main__":
         cur = os.stat('./input').st_mtime
         if cur != prev:
             modified()
-            prev=cur
+            prev = cur
         time.sleep(2)
-   
+
+    """
     # For testing in development phase
-    # import json
-    # data_dir = "./data"
-    # save_dir = "./output"
-    # os.makedirs(save_dir, exist_ok=True)
-    # for filename in os.listdir(data_dir):
-    #     fpath = os.path.join(data_dir, filename)
-    #     if not os.path.isfile(fpath):
-    #         continue
-    #     print("Running with", fpath)
-    #     basename = os.path.splitext(filename)[0]
-    #     context = _extract_text(fpath)
-    #     with open(os.path.join(save_dir, basename + "_text.txt"), "w") as f:
-    #         f.write(context)
-    #     details = _extract_details(context)
-    #     with open(os.path.join(save_dir, basename + "_pred.json"), "w") as f:
-    #         f.write(json.dumps(details))
+    import json
+    data_dir = "./data"
+    save_dir = "./output"
+    os.makedirs(save_dir, exist_ok=True)
+    for filename in os.listdir(data_dir):
+        fpath = os.path.join(data_dir, filename)
+        if not os.path.isfile(fpath):
+            continue
+        print("Running with", fpath)
+        basename = os.path.splitext(filename)[0]
+        img = cv2.imread(fpath)
+        context = _extract_text(img)
+        with open(os.path.join(save_dir, basename + "_text.txt"), "w") as f:
+            f.write(context)
+        details = _extract_details(context)
+        with open(os.path.join(save_dir, basename + "_pred.json"), "w") as f:
+            f.write(json.dumps(details))
+    """
